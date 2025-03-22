6ed50c176ebc3fa384ac35c42c317222c466d7d2
Josh Levinson <joshua.levinson@gmail.com>
2025-03-21 23:14:10 -0400

    Very basic joke query

--- src/app/jokes/page.tsx
+++ src/app/jokes/page.tsx
@@ -1,7 +1,6 @@
-import { createClient } from '@/utils/supabase';
+import { supabase } from '@/lib/client';
 
 export default async function Jokes() {
-  const supabase = await createClient();
   const { data: jokes } = await supabase.from("jokes").select();
 
   return <pre>{JSON.stringify(jokes, null, 2)}</pre>
